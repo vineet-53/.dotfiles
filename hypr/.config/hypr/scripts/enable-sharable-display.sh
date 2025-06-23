934dbf3e0ff9ff964dc91deb04d172363cac73ec
vineet-53 <vineetsinghnegi53@gmail.com>
2025-06-23 12:21:05 -0400

    updated: waybar configs

--- hypr/.config/hypr/scripts/enable-sharable-display.sh
+++ hypr/.config/hypr/scripts/enable-sharable-display.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cat <(cat "$HOME/.config/hypr/monitor_profiles/hdmi-enable.conf" "$HOME/.config/hypr/monitor_profiles/edp-enable.conf") > $HOME/.config/hypr/monitors.conf
+cat <(cat "$HOME/.config/hypr/monitor_profiles/extended.conf") > $HOME/.config/hypr/monitors.conf
 hyprctl reload
 
